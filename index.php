5bf290638c986022cb2b250d32556d4978fd04f0
bpeterson1663 <bpeterson1663@gmail.com>
2016-08-31 23:27:54 -0500

    Added Favicon to all pages

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="images/favicon.png">
+    <link rel="icon" type="image/png"  href="images/favicon.png">
 
     <title>Hickory Grove Golf Course</title>
 
