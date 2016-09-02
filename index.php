d0886b5cbbb1465f27116a42c0462d9405187ec4
bpeterson1663 <bpeterson1663@gmail.com>
2016-09-01 20:31:53 -0500

    Added Meta tags

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="Description" content="Hickory Grove Golf Course is a beautiful nine-hole golf course located in the heart of Southwest Wisconsin. Located just a few miles west of Fennimore, WI Hickory Grove Golf Course offers great service at great prices.">
+    <meta name="Author" content="Brady Peterson">
+    <meta name="Keywords" content="golf, golf course, wisconsin, afordable golf, driving range, practice range">
+    <meta name="Robots" content="all">
     <link rel="icon" type="image/png"  href="images/favicon.png">
 
     <title>Hickory Grove Golf Course</title>
