635ceb69b770e914549661b714344ffcb2fcd30d
bpeterson1663 <bpeterson1663@gmail.com>
2016-09-04 09:41:02 -0500

    Continue Work on Drop Down Nav

--- index.php
+++ index.php
@@ -60,6 +60,37 @@
             </div>
           </div>
          <!--Drop Down Menu--> 
+
+      <!--The Golf Course, Rates, Practice Facilities, Lessons-->
+      <!--NEED TO INCLUDE JAVASCRIPT AND CSS-->
+      <!--<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" data-hover="dropdown">
+   Dropdown <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li class="dropdown">
+      <a href="#">One more dropdown</a>
+      <ul class="dropdown-menu">
+        <li><a href="#">Action</a></li>
+        <li><a href="#">Another action</a></li>
+        <li class="dropdown">
+          <a href="#">One more dropdown</a>
+          <ul class="dropdown-menu">
+          ...
+          </ul>
+        </li>
+        <li><a href="#">Something else here</a></li>
+        <li><a href="#">Separated link</a></li>
+       </ul>
+    </li>
+    <li><a href="#">Something else here</a></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>-->
+
+
           <div class="inner cover">
             <img src="images/HG-Logo.png" alt="Hickory Grove Logo" class="main-logo"/>  
             <h1 class="cover-heading">Welcome To Hickory Grove Golf Course</h1>
--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
     $(".cover").fadeIn("slow");
+    $('.dropdown-toggle').dropdown()
+    
 });
