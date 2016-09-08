0dc6bef3d8693d5f4d35a02c237fb80eb23635cf
bpeterson1663 <bpeterson1663@gmail.com>
2016-09-07 22:04:22 -0500

    Sync with master

--- index.php
+++ index.php
@@ -51,33 +51,6 @@
                    <div class="collapse navbars-collapse" id="myNavbar">
                       <ul class="nav navbar-nav masthead-nav">
 
-                        <div class="dropdown">
-                          <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" data-hover="dropdown">
-                          Home <span class="caret"></span>
-                          </button>
-                          <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li class="dropdown">
-                              <a href="#">One more dropdown</a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li class="dropdown">
-                                  <a href="#">One more dropdown</a>
-                                  <ul class="dropdown-menu">
-                                  ...
-                                  </ul>
-                                </li>
-                                <li><a href="#">Something else here</a></li>
-                                <li><a href="#">Separated link</a></li>
-                              </ul>
-                            </li>
-                            <li><a href="#">Something else here</a></li>
-                            <li><a href="#">Separated link</a></li>
-                          </ul>
-                        </div>
-
                           <!--<li><a href="index.html">Home</a></li>-->
                           <li><a href="views/course.html">The Course</a></li>
                           <li><a href="views/events.html">Events</a></li>
